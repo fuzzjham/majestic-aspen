1f9b3abc0e03cd03146598c928fe944f6d5f496e
John Hamson <jhamson@fuzzproductions.com>
2021-01-25 06:28:33 -0600

    maskable offline icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         background_color: `#6C63FF`,
         theme_color: `#6C63FF`,
         display: `minimal-ui`,
-        icon: `src/assets/images/favicon.png`,
+        icon: `src/assets/images/offline-icon.png`,
       },
     },
     'gatsby-plugin-offline',
